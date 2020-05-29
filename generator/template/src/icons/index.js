635cbc6f47df60e4b227abc1cdc1208e059c99d1
zhouzhili <zhouzhili@360.cn>
2020-05-29 18:14:03 +0800

    使用vue-element-admin模板

--- generator/template/src/icons/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from 'vue-svgicon'
-import './svg-icon.less'
-
-Vue.use(SvgIcon, {
-  tagName: 'svg-icon',
-  defaultWidth: '1em',
-  defaultHeight: '1em'
-})
--- ui/vant/src/vendor/vant.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import vant from 'vant'
-import 'vant/lib/index.css'
-
-Vue.use(vant)
